dbba7c6e8e1875211be193dfbf565bb29e5ec630
Christopher Bridges <wonkydata@gmail.com>
2026-07-07 12:17:18 -0700

    Rename file from LadyBug to VandalAnalytics

--- VandalAnalytics_datalayer_to_bigquery.py
+++ VandalAnalytics_datalayer_to_bigquery.py
@@ -1,6 +1,6 @@
 # Wonkydata.com LLC
 #
-# LadyBug_datalayer_to_bigquery.py
+# VandalAnalytics_datalayer_to_bigquery.py
 # - GCP Cloud Run function to process dataLayer captures, sent from a GTM tag template. 
 #
 # Christopher Bridges
