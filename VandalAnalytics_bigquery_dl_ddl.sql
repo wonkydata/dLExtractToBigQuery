9996fe252c300ca18fd61cb20e22c534bc832ae0
Christopher Bridges <wonkydata@gmail.com>
2026-07-07 12:17:52 -0700

    Rename file header to VandalAnalytics_bigquery_dl_ddl.sql
    Updated file header to reflect correct file name.

--- VandalAnalytics_bigquery_dl_ddl.sql
+++ VandalAnalytics_bigquery_dl_ddl.sql
@@ -1,6 +1,6 @@
 -- Wonkydata.com LLC
 --
--- LadyBug_bigquery_dl_ddl.sql
+-- VandalAnalytics_bigquery_dl_ddl.sql
 -- - DDL to create analytics_raw/datalayer_events schema and table . 
 -- - Change the dataset name.
 -- Christopher Bridges
