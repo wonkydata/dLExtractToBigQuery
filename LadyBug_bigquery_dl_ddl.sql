94a65df9682892124d3f7c61fc3579a5d1b107b8
Christopher Bridges <wonkydata@gmail.com>
2026-07-27 16:00:19 -0700

    Refactor BigQuery table schema in LadyBug DDL
    Removed unnecessary fields from the schema and reordered some fields.

--- LadyBug_bigquery_dl_ddl.sql
+++ LadyBug_bigquery_dl_ddl.sql
@@ -16,16 +16,11 @@ CREATE TABLE IF NOT EXISTS `bigquery-ga360-99999.analytics_raw.datalayer_events`
   event_name STRING,
   page_location STRING,
   page_title STRING,
-  gtm_container_id STRING,
-  gtm_workspace_id STRING,
-  user_agent STRING,
-  ip_hash STRING,
-  user_id STRING,
-  page_type STRING,
-  campaign STRING,
-  ecommerce JSON,
   dl_json JSON,
-  full_payload_json JSON
+  full_payload_json JSON,
+  user_agent STRING,
+  ip_hash STRING
+
 )
 PARTITION BY DATE(received_at)
 CLUSTER BY event_name, page_location, page_type;
